166973e06ee64fa214dbe8956f5c027bbd7c415a
scherepi <scherejoaquin@gmail.com>
2024-12-10 12:56:52 -0600

    Added log check to squash bugs.

--- wokPad.py
+++ wokPad.py
@@ -58,14 +58,17 @@ except:
 	exit(0)
 
 try: 
+	# If everything is set up properly, we can go ahead and initalize our log files for writing.
 	logFile = open(logDestination, "a")
 	rawKeyFile = open(rawKeyDestination, "a")
-	rawKeyFile.write("-- start of session --\n")
+	rawKeyFile.write("\n-- start of session --\n")
 	logging = True
 except:
+	# Aw man, logs are messed up :(
 	print("Unable to initialize log file, logging will be disabled.")
 
 def checkCode():
+	# Simple function to check if the input matches our set PIN.
 	global secretCode
 	global currentInput
 	if (secretCode == currentInput):
@@ -80,34 +83,42 @@ def readLine(line, characters):
 	if (GPIO.input(C1) == 1):
 		#print(characters[0] + " || " + str(line) + "C1")
 		currentInput += characters[0]
-		rawKeyFile.write(characters[0])
+		if (logging):
+			rawKeyFile.write(characters[0]) # Log raw keypresses
 		print("> " + currentInput)
 	if (GPIO.input(C2) == 1):
 		#print(characters[1] + " || " + str(line) + "C2")
 		currentInput += characters[1]
-		rawKeyFile.write(characters[1])
+		if (logging):
+			rawKeyFile.write(characters[1]) # Continue to log raw keypresses...
 		print("> " + currentInput)
 	if (GPIO.input(C3) == 1):
 		#print(characters[2] + " || " + str(line) + "C3")
-		rawKeyFile.write(characters[2])
+		if (logging):
+			rawKeyFile.write(characters[2]) # Everything has to go in the raw logs.
 		if (line == L4):
+			# All of this will run when the 'C' button is pressed.
 			if (checkCode()):
 				print("Congratulations! You got the code!")
+				#TODO: implement more interesting success behavior (LED or piezo, perhaps)
 				exit(0)
 			else:
 				print("Incorrect code.")
 				currentIncorrect += 1
 				if (logging):
+					# If log initialization was successful and you messed up, we're keeping track.
 					logFile.write("FAILED ATTEMPT: " + currentInput + "\n")
 				if (currentIncorrect == maxTries):
+					# If you messed up enough, you're getting locked out! And we're writing a special event to the logs, just to track what an embarassment you are.
 					logFile.write("!!! LOCKOUT EVENT !!! @ " + now.__str__())
-					print("Nice try. You aren't brute-forcing this.")
+					print("Nice try. You aren't brute-forcing this.") # Gotta be a little cheeky - attitude is part of the hacker toolkit
 					exit(0)
 		else:
 			currentInput += characters[2]
 			print("> " + currentInput)
 	if (GPIO.input(C4) == 1):
-		rawKeyFile.write(characters[3])
+		if (logging):
+			rawKeyFile.write(characters[3])
 		if (line == L3):
 			print("Input cleared!")
 			currentInput = ""
